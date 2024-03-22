a22f4337d3cc965f6de3ef94af5224561a2e165e
CarlBerthold <carl_berthold@posteo.de>
2024-03-22 19:11:25 +0100

    making the query function dynamic, it need to be called with an argument which is a mysql query

--- index.php
+++ index.php
@@ -5,42 +5,33 @@ require 'functions.php';
 require 'router.php';
 
 
-//connect to my Mysql database via class as blueprint;
 
-// using dsn with all credentials to connect to the database
-$dsn = "mysql:host=localhost;port=3306;dbname=section_2;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
+class Database {
+    public function query($query) {
+        //connect to my Mysql database via class as blueprint;
+
+        // using dsn with all credentials to connect to the database
+        $dsn = "mysql:host=localhost;port=3306;dbname=section_2;user=root;charset=utf8mb4";
+        $pdo = new PDO($dsn);
 
 
-//query the database for all posts an run it
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
+        //query the database for all posts an run it
+        $statement = $pdo->prepare($query);
+        $statement->execute();
 
-//save all the posts in a variable and loop through them
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        //save all the posts in a variable and loop through them
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$db = new Database;
+$posts = $db->query("select * from posts");
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
 }
 
 
-/* class Person {
-    public $name;
-    public $age;
 
-    public function breathe() {
-        echo $this->name . 'am breathing';
-    }
-
-    public $gender;
-}
-
-$person = new Person();
-$person->name = "John";
-$person->age = 30;
-$person->gender = "male";
-
-
-dd($person->breathe()); */
 
 ?>
