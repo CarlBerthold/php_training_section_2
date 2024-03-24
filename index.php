7cfff6c347cbd3b290c55021b435ea6584a097a2
CarlBerthold <carl_berthold@posteo.de>
2024-03-24 10:27:17 +0100

    move Database instance to single file

--- index.php
+++ index.php
@@ -2,35 +2,16 @@
 
 <?php 
 require 'functions.php';
-require 'router.php';
+require 'Database.php';
+// require 'router.php';
 
 
-//Database Instance
-class Database {
 
-    public $connection;
-    //runs immediately when the class is called to set a new PDO connection
-
-    public function __construct() {
-        // PDO instance ass connection Instance property 
-        $dsn = "mysql:host=localhost;port=3306;dbname=section_2;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
-
-    }
-    public function query($query) {
-        
-        //query the database for all posts an run it
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        //save all the posts in a variable and loop through them
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
 
 $db = new Database;
 // calling the query with the mysql query
-$posts = $db->query("select * from posts");
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+//dd($posts);
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
